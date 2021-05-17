943092855619d3554e879f28e34740c815b53c14
prynsh7 <priyanshraj45@gmail.com>
2021-05-17 19:27:07 +0530

    Navbar completed

--- Desktop/my_first_app/src/App.js
+++ Desktop/my_first_app/src/App.js
@@ -1,12 +1,10 @@
 
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar.js";
 function App() {
   return (
-    <div className="app">
-App
-    </div>
+    <Navbar />
   );
 }
 
