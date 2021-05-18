e66f07707600c81ba5915fe977ed2739fab40c25
prynsh7 <priyanshraj45@gmail.com>
2021-05-18 18:14:24 +0530

    Header Complete

--- Desktop/my_first_app/src/App.js
+++ Desktop/my_first_app/src/App.js
@@ -1,10 +1,46 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar.js";
+import Particles from "react-particles-js";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 function App() {
   return (
+    <>
+    <Particles 
+    params={{
+      particles:{
+      number:{
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 900
+        }
+    }, color: {
+      value: '#000'},
+      opacity: {
+        value: 0.1,
+        anim: {
+            enable: true
+        }
+    },
+    line_linked: {
+      color: '#f44242',
+      opacity: 1
+    },
+    shape: {
+      type: "star",
+      stroke: {
+        width: 6,
+        color: "#f9ab00"
+      }
+    }
+  }}}
+    />
     <Navbar />
+    <Header/>
+
+    </>
   );
 }
 
